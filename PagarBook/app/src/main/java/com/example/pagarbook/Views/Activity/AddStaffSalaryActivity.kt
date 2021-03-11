07764fff12a119172da50569bb65ceff0d27c973
manishkumar <manishsahu354@gmail.com>
2021-03-11 20:35:28 +0530

    added add staff activities

---
+++ PagarBook/app/src/main/java/com/example/pagarbook/Views/Activity/AddStaffSalaryActivity.kt
@@ -0,0 +1,40 @@
+package com.example.pagarbook.Views.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pagarbook.R
+import kotlinx.android.synthetic.main.activity_add_staff.*
+import kotlinx.android.synthetic.main.activity_add_staff_salary.*
+
+class AddStaffSalaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_staff_salary)
+
+        val staffName = intent.getStringExtra("staffName")
+        val staffNumber = intent.getStringExtra("staffNumber")
+
+        btnBackAddStaffSalary.setOnClickListener {
+            finish()
+        }
+
+        btnContinueAddStaffSalary.setOnClickListener {
+
+            if (et_staff_amount.text.toString().isNotEmpty() ){
+
+                val intent = Intent(this,OpeningBalance::class.java)
+                intent.putExtra("staffAmount",et_staff_amount.text.toString())
+
+                intent.putExtra("staffName",staffName)
+                intent.putExtra("staffNumber",staffNumber)
+
+                startActivity(intent)
+
+            }else{
+                et_staff_amount.error = "please Enter"
+
+            }
+        }
+    }
+}
--- PagarBook/app/src/main/java/com/example/pagarbook/Views/Activity/OpeningBalance.kt
+++ PagarBook/app/src/main/java/com/example/pagarbook/Views/Activity/OpeningBalance.kt
@@ -1,12 +1,30 @@
 package com.example.pagarbook.Views.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pagarbook.R
+import kotlinx.android.synthetic.main.activity_add_staff_salary.*
+import kotlinx.android.synthetic.main.activity_opening_balance.*
 
 class OpeningBalance : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opening_balance)
+
+        val staffName = intent.getStringExtra("staffName")
+        val staffNumber = intent.getStringExtra("staffNumber")
+
+        val staffAmount = intent.getStringExtra("staffAmount")
+
+
+        btnContinueOpeningBalance.setOnClickListener {
+            val intent = Intent(this,DashboardActivity::class.java)
+            intent.putExtra("openingBalance",et_opening_balance.text.toString())
+            intent.putExtra("staffName",staffName)
+            intent.putExtra("staffNumber",staffNumber)
+            intent.putExtra("staffAmount",staffAmount)
+            startActivity(intent)
+        }
     }
 }
