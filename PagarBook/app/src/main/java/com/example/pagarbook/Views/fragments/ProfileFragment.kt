441ccc2eb5715966cecfa353fb3897a953b816c6
ravi.rdx <ravi.solanki146783@gmail.com>
2021-03-12 10:35:41 +0530

    Edit profile and edit business Name

--- PagarBook/app/src/main/java/com/example/pagarbook/Views/fragments/ProfileFragment.kt
+++ PagarBook/app/src/main/java/com/example/pagarbook/Views/fragments/ProfileFragment.kt
@@ -1,17 +1,20 @@
 package com.example.pagarbook.Views.fragments
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.pagarbook.DataNConstants.ConstantsNData
 import com.example.pagarbook.R
 import com.example.pagarbook.Views.Activity.EditBusinessNameActivity
 import com.example.pagarbook.Views.Activity.EditUserNameActivity
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
+
 
 class ProfileFragment : Fragment() {
 
@@ -28,10 +31,8 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.tvProfileLogout.setOnClickListener {
-            cn.setLoginStatus(context,"0")
+            cn.setLoginStatus(context, "0")
             System.exit(1)
-
-
         }
 
         view.tvProfileYourName.text = cn.getUserName(context)
@@ -39,18 +40,20 @@ class ProfileFragment : Fragment() {
         view.tvProfileNumberOfStaff.text = cn.getStaffs(context)
 
         view.tvEditYourName.setOnClickListener {
-            val intent=Intent(context,EditUserNameActivity::class.java)
+            val intent=Intent(context, EditUserNameActivity::class.java)
             var name= cn.getUserName(context)
             intent.putExtra("name", name)
             startActivity(intent)
         }
         view.tvEditBussinessName.setOnClickListener {
-            val intent=Intent(context,EditBusinessNameActivity::class.java)
+            val intent=Intent(context, EditBusinessNameActivity::class.java)
             var business= cn.getBussinessName(context)
-            intent.putExtra("business",business)
-            intent.putExtra("staff",cn.getStaffs(context))
+            intent.putExtra("business", business)
+            intent.putExtra("staff", cn.getStaffs(context))
             startActivity(intent)
         }
 
     }
+
+
 }
